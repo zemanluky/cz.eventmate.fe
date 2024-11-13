9407bca1ebb7442c58ca7cad71854e5fa57354cc
Vlada <vladam3636@gmail.com>
2024-11-13 20:31:26 +0100

    Added Avatar park-ui component and implemented in Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,45 +1,57 @@
 import * as React from "react";
-import { useState } from "react";
 import { Button } from "@ParkComponents/button.tsx";
 import { css } from "@Panda/css";
 import { Input } from "@ParkComponents/input";
 import { Text } from "@ParkComponents/text";
-
+import { Avatar } from "@ParkComponents/avatar";
 
 export const Navbar: React.FC = () => {
-	const navBarStyles = css({
-		w: "100%",
-		h: "100px",
-		paddingX: { base: "16px", sm: "48px" },
-		display: "flex",
-        gap:"60px",
-		alignItems: "center",
-		bg: "bg.navbar",
-		boxShadow: "0px 4px 1px 0px var(--colors-neutrals-olive-3, #EFF1EF)",
-	});
+  const navBarStyles = css({
+    w: "100%",
+    h: "100px",
+    paddingX: { base: "16px", sm: "48px" },
+    display: "flex",
+    gap: "60px",
+    alignItems: "center",
+    bg: "bg.navbar",
+    boxShadow: "0px 4px 1px 0px var(--colors-neutrals-olive-3, #EFF1EF)",
+  });
 
-    const inputStyles = css({
-        width:"100%",
-        border:"1px solid #EFF1EF",
-        color:"card"
-    })
+  const inputStyles = css({
+    width: "100%",
+    border: "1px solid #EFF1EF",
+    color: "card",
+  });
 
-	const flexStyles = css({
-		display: "flex",
-		alignItems: "center",
-		gap:"20px"
-	})
+  const flexStyles = css({
+    display: "flex",
+    alignItems: "center",
+    gap: "20px",
+  });
 
-	return (
-		<>
-			<div className={navBarStyles}>
-				<Text fontSize={"xl"}>EventMate</Text>
-                <Input size="sm" id="name" placeholder="Search" className={inputStyles}/>
-                <div className={flexStyles}>
-					<Button bg="bg.buttonSmall" color="fg.buttonSmall" borderRadius={"full"}>F</Button>
-					<Button variant="ghost" bg="none" borderRadius={"full"} border={"1px solid gray"}>P</Button>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={navBarStyles}>
+        <Text fontSize={"xl"}>EventMate</Text>
+        <Input
+          size="sm"
+          id="name"
+          placeholder="Search"
+          className={inputStyles}
+        />
+        <div className={flexStyles}>
+          <Button
+            bg="bg.buttonSmall"
+            color="fg.buttonSmall"
+            borderRadius={"full"}
+          >
+            F
+          </Button>
+          <Button variant="ghost" bg="none" borderRadius={"full"} p="0">
+            <Avatar name="John Doe" />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
