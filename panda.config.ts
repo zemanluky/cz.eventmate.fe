2d6418c0c44e9312a0f242922f6fde667901a3ef
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-13 11:11:07 +0100

    Added color theme (buttons, nav, card)

--- panda.config.ts
+++ panda.config.ts
@@ -8,11 +8,35 @@ export default defineConfig({
   presets: [
     '@pandacss/preset-base',
     createPreset({
-      accentColor: "neutral",
-      grayColor: "neutral",
+      accentColor: "grass",
+      grayColor: "olive",
       borderRadius: 'md'
     })
   ],
+  // Custom themes for coloring
+  theme: {
+    extend: {
+      semanticTokens: {
+        colors: {
+          fg: {
+            cancel: { value: '{colors.olive.12}' },     // cancel button color 
+            buttonLarge: {value: '{#FFFFFF}'},          // large button color 
+            buttonSmall: {value: '{colors.olive.12}'},  // small button color
+            card: {value: "{colors.olive.11}"},         // event cards text color (date,location)
+            cardHeading: {value: "{#000000}"}           // event cards text color (heading)
+
+          },
+          bg: {
+            cancel: { value: '{colors.olive.7}'},     // cancel button background color 
+            buttonLarge: {value: '{colors.grass.8}'}, // large button background color 
+            buttonSmall: {value: '{colors.lime.9}'},  // small button background color
+            card: {value: "{colors.olive.2}"},        // event cards background color
+            navbar: {value: "{colors.olive.2}"}       // nav background color
+          },
+        },
+      },
+    },
+  },
   // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
   // Files to exclude
