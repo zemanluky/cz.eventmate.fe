66e94196e980ffd1eec644ab26c2600f2551cfe3
Vlada <vladam3636@gmail.com>
2024-11-16 21:16:42 +0100

    Added new button color

--- panda.config.ts
+++ panda.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig } from "@pandacss/dev";
-import {createPreset} from "@park-ui/panda-preset";
+import { createPreset } from "@park-ui/panda-preset";
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
   // preset styles
   presets: [
-    '@pandacss/preset-base',
+    "@pandacss/preset-base",
     createPreset({
       accentColor: "grass",
       grayColor: "olive",
-      borderRadius: 'md'
+      borderRadius: "md",
     }),
     createPreset({
       accentColor: "lime",
       grayColor: "olive",
-      borderRadius: 'md'
-    })
+      borderRadius: "md",
+    }),
   ],
   // Custom themes for coloring
   theme: {
@@ -24,19 +24,19 @@ export default defineConfig({
       semanticTokens: {
         colors: {
           fg: {
-            cancel: { value: '{colors.olive.12}' },     // cancel button color 
-            buttonLarge: {value: '{#FFFFFF}'},          // large button color 
-            buttonSmall: {value: '{colors.olive.12}'},  // small button color
-            card: {value: "{colors.olive.11}"},         // event cards text color (date,location)
-            cardHeading: {value: "{#000000}"}           // event cards text color (heading)
-
+            cancel: { value: "{colors.olive.12}" }, // cancel button color
+            buttonLarge: { value: "{#FFFFFF}" }, // large button color
+            buttonSmall: { value: "{colors.olive.12}" }, // small button color
+            card: { value: "{colors.olive.11}" }, // event cards text color (date,location)
+            cardHeading: { value: "{#000000}" }, // event cards text color (heading)
           },
           bg: {
-            cancel: { value: '{colors.olive.7}'},     // cancel button background color 
-            buttonLarge: {value: '{colors.grass.8}'}, // large button background color 
-            buttonSmall: {value: '{colors.lime.9}'},  // small button background color
-            card: {value: "{colors.olive.2}"},        // event cards background color
-            navbar: {value: "{colors.olive.2}"}       // nav background color
+            reject: { value: "#ec8e7b" }, // reject button background color
+            cancel: { value: "{colors.olive.7}" }, // cancel button background color
+            buttonLarge: { value: "{colors.grass.8}" }, // large button background color
+            buttonSmall: { value: "{colors.lime.9}" }, // small button background color
+            card: { value: "{colors.olive.2}" }, // event cards background color
+            navbar: { value: "{colors.olive.2}" }, // nav background color
           },
         },
       },
@@ -49,5 +49,5 @@ export default defineConfig({
   // The output directory for your css system
   outdir: "styled-system",
   // this is needed for creating components with panda styles
-  jsxFramework: "react"
+  jsxFramework: "react",
 });
