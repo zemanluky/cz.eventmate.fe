643e19c8616e409833de0fb5547f27435699142c
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-20 22:06:55 +0100

    Updated router with ProfilePage route

--- src/core/Router.tsx
+++ src/core/Router.tsx
@@ -1,29 +1,37 @@
 import * as React from "react";
-import {createHashRouter, RouteObject, RouterProvider} from "react-router-dom";
-import {PageWrapper} from "@Components/layout";
-import {Homepage} from "@Pages";
-import {AuthPage} from "@Pages";
+import {
+	createHashRouter,
+	RouteObject,
+	RouterProvider,
+} from "react-router-dom";
+import { PageWrapper } from "@Components/layout";
+import { Homepage, ProfilePage } from "@Pages";
+import { AuthPage } from "@Pages";
 
 const routes: RouteObject[] = [
-    {
-        path: '/',
-        element: <PageWrapper/>,
-        children: [
-            {
-                path: '',
-                element: <Homepage/>
-            },
-            {
-                path: 'auth',
-                element: <AuthPage/> /* 👈 Renders at /#/auth/ */
-            },
-            // add more routes here...
-        ]
+	{
+		path: "/",
+		element: <PageWrapper />,
+		children: [
+			{
+				path: "",
+				element: <Homepage />,
+			},
+			{
+				path: "auth",
+				element: <AuthPage /> /* 👈 Renders at /#/auth/ */,
+			},
+			{
+				path: "profile",
+				element: <ProfilePage /> /* 👈 Renders at /#/profile/ */,
+			},
 
-    },
-    // add different page layout...
+			// add more routes here...
+		],
+	},
+	// add different page layout...
 ];
 // hash router is necessary here because otherwise it would not work on GitHub pages
 const router = createHashRouter(routes);
 
-export const Router: React.FC = () => <RouterProvider router={router} />
+export const Router: React.FC = () => <RouterProvider router={router} />;
