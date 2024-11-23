9478a176a6f95c331fcccdf556f6891bc2744262
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-11-23 21:59:38 +0100

    Configure .env import for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import * as path from "node:path";
+import { config } from 'dotenv';
+config()
 
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
   plugins: [
     tsconfigPaths({ root: './' }),
     react()
