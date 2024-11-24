62f88ca93f6cb77bd5bef8a9c8172d1fc728f6b5
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-11-24 22:38:40 +0100

    (auth) implemented auth/register endpoint request

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -3,12 +3,50 @@ import { Button } from "@ParkComponents/button";
 import { FormLabel } from "@ParkComponents/form-label";
 import { Input } from "@ParkComponents/input";
 import { Text } from "@ParkComponents/text";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Eye, EyeOff } from "lucide-react";
 import { Icon } from "@ParkComponents/icon";
 
 import * as React from "react";
+import { IconButton } from "@ParkComponents/icon-button";
+import axios from "axios";
 
 export const LoginForm: React.FC = () => {
+
+  const [inputs, setInputs] = React.useState({
+    email:"",
+    password:""
+  })
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleSubmit = async() =>{
+    // Ensure all fields are filled
+		if (
+			!inputs.email ||
+			!inputs.password
+		) {
+			alert("Please fill all fields correctly.");
+			return;
+		}
+
+    const formData = {...inputs}
+
+    try {
+			const response = await axios.post(`${import.meta.env.VITE_API_KEY}/auth/login`, formData);
+			console.log("Login successful:", response.data);
+		
+			alert("Login successful!");
+		  } catch (error) {
+			// Check for server errors or network issues
+			if (axios.isAxiosError(error)) {
+				console.error("Error:", error.response?.data || error.message);
+				alert(`Login failed: ${error.response?.data?.message || error.message}`);
+			  } else {
+				console.error("Unexpected error:", error);
+				alert("An unexpected error occurred. Please try again later.");
+			  }
+		  }
+  }
+
   return (
     <VStack w= {{base: "350px", sm:"450px"}} mt={{base: "15px", sm:"15px"}}>
       <Text size="6xl" mb="10px">
@@ -16,13 +54,40 @@ export const LoginForm: React.FC = () => {
       </Text>
       <Stack mb="0.69rem" w="100%">
         <FormLabel fontWeight="bold">E-mail</FormLabel>
-        <Input placeholder="E-mail" />
+        <Input
+					value={inputs.email}
+					size="md"
+					w="100%"
+					id="email"
+					placeholder="E-mail"
+					onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+				/>
       </Stack>
       <Stack mb="2rem" w="100%">
         <FormLabel fontWeight="bold">Password</FormLabel>
-        <Input placeholder="Password" />
+        <Flex alignItems={"center"} gap="8px">
+          <Input
+              value={inputs.password}
+              size="md"
+              w="100%"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
+            />
+            <IconButton
+              onClick={() => setShowPassword(!showPassword)}
+              variant={"outline"}
+            >
+              {showPassword ? <Eye /> : <EyeOff />}
+            </IconButton>
+        </Flex>
       </Stack>
-      <Button bg="bg.buttonLarge" w="100%">
+      <Button bg="bg.buttonLarge" w="100%" 
+      disabled={!inputs.email || !inputs.password}
+      onClick={handleSubmit}>
         <Text color="white">Sign In</Text>
         <Icon>
           <ArrowRight color="white" />
