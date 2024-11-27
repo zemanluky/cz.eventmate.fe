1e14751a4112e3fa202f2ab45db8ab474ff6af5c
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-11-27 09:13:18 +0100

    Added ToastProvider to Providers

--- src/core/Providers.tsx
+++ src/core/Providers.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import {ReactNode} from "react";
+import { ToastProvider } from "src/providers/ToastProvider";
 
 export const Providers: React.FC<{children?: ReactNode}> = ({children}) => {
     // put context providers here...
 
     return <>
+        <ToastProvider />
         {children}
     </>;
 }
