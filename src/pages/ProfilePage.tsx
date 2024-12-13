bc4fc06a7cfce979fa5f99a6e6a43ef7bbb3f1bb
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-12-13 20:40:21 +0100

    Added fetching user data by id on profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -7,12 +7,16 @@ import { EventCardBigDesktop, Profile, RatingCard } from "@Components/ui";
 import { Box, Divider, Flex, VStack } from "@Panda/jsx";
 import { Text } from "@ParkComponents/text";
 import { Button } from "@ParkComponents/button";
-//import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import useGetUserById from "src/hooks/useGetUserById";
+import { Spinner } from "@ParkComponents/spinner";
 
 export const ProfilePage: React.FC = () => {
-  //const params = useParams();
-  //const userId = params.userId as string;
-  //const user = handleGetUser(userId) */
+  const params = useParams();
+  const userId = params.userId as string;
+
+  const { fetchUser, user, loading, error } = useGetUserById(userId);
+  console.log(user);
 
   const mockEvents = [
     {
@@ -151,48 +155,6 @@ export const ProfilePage: React.FC = () => {
   const [showEvents, setShowEvents] = React.useState(true);
   const showToast = useShowToast();
 
-  const getProfileDetails = async () => {
-    try {
-      const token = localStorage.getItem("authToken");
-
-      if (!token) {
-        showToast(
-          "Auth Error",
-          "User is not authenticated. Please log in.",
-          "error"
-        );
-        return;
-      }
-
-      // Make the profile request
-      const response = await axiosClient.get(
-        `${import.meta.env.VITE_API_KEY}/user/profile`,
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      console.log("Profile data acquired successfully!", response.data);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        showToast(
-          "Error",
-          `Fetching data failed: ${error.response?.data?.message || error.message}`,
-          "error"
-        );
-      } else {
-        showToast("Unexpected Error", "Please try again later", "error");
-      }
-    }
-  };
-
-  useEffect(() => {
-    getProfileDetails(); // Attempt to get profile data on component mount
-  }, []);
-
   const mockUser = {
     id: "4",
     name: "Bob",
@@ -201,54 +163,73 @@ export const ProfilePage: React.FC = () => {
     rating: 3.5,
   };
 
+  useEffect(() => {
+    fetchUser(userId);
+  }, [userId]);
+
   return (
-    <Box>
-      <Profile user={mockUser} />
-      <Divider orientation="horizontal" thickness="2px" mt="10px" mb="10px" />
-      {/* Switch */}
-      <Box w="150px" p="5px" h="36px" rounded="full" bg="bg.muted">
-        <Flex justifyItems="center">
-          <Button
-            w="70px"
-            h="26px"
-            rounded="full"
-            bg={showEvents ? "bg.buttonSmall" : "transparent"}
-            onClick={() => setShowEvents(true)}
-          >
-            <Text>Events</Text>
-          </Button>
-          <Button
-            w="70px"
-            h="26px"
-            rounded="full"
-            bg={showEvents ? "transparent" : "bg.buttonSmall"}
-            onClick={() => setShowEvents(false)}
-          >
-            <Text>Ratings</Text>
-          </Button>
-        </Flex>
-      </Box>
-      {/* Content (Events or Ratings) */}
-      <Box>
-        {showEvents ? (
-          <Flex
-            gap="16px"
-            mt={"20px"}
-            flexWrap={"wrap"}
-            justifyContent="center"
-          >
-            {mockEvents.map((event) => (
-              <EventCardBigDesktop key={event._id} event={event} />
-            ))}
-          </Flex>
-        ) : (
-          <VStack mt="20px" gap="16px">
-            {mockRatings.map((rating, index) => (
-              <RatingCard key={index} rating={rating} />
-            ))}
-          </VStack>
-        )}
-      </Box>
-    </Box>
+    <>
+      {loading ? (
+        <Box display="grid" placeItems="center" height={`calc(100vh - 150px)`}>
+          <Spinner />
+        </Box>
+      ) : (
+        <>
+          <Box>
+            <Profile user={user} />
+            <Divider
+              orientation="horizontal"
+              thickness="2px"
+              mt="10px"
+              mb="10px"
+            />
+            {/* Switch */}
+            <Box w="150px" p="5px" h="36px" rounded="full" bg="bg.muted">
+              <Flex justifyItems="center">
+                <Button
+                  w="70px"
+                  h="26px"
+                  rounded="full"
+                  bg={showEvents ? "bg.buttonSmall" : "transparent"}
+                  onClick={() => setShowEvents(true)}
+                >
+                  <Text>Events</Text>
+                </Button>
+                <Button
+                  w="70px"
+                  h="26px"
+                  rounded="full"
+                  bg={showEvents ? "transparent" : "bg.buttonSmall"}
+                  onClick={() => setShowEvents(false)}
+                >
+                  <Text>Ratings</Text>
+                </Button>
+              </Flex>
+            </Box>
+            {/* Content (Events or Ratings) */}
+            <Box>
+              {showEvents ? (
+                <Flex
+                  gap="16px"
+                  mt={"20px"}
+                  flexWrap={"wrap"}
+                  justifyContent="center"
+                >
+                  {mockEvents.map((event) => (
+                    <EventCardBigDesktop key={event._id} event={event} />
+                  ))}
+                </Flex>
+              ) : (
+                <VStack mt="20px" gap="16px">
+                  {mockRatings.map((rating, index) => (
+                    <RatingCard key={index} rating={rating} />
+                  ))}
+                </VStack>
+              )}
+            </Box>
+          </Box>
+        </>
+      )}
+    </>
   );
 };
