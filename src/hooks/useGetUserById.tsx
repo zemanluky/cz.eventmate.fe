732d718cd5a917e78821dddc00d24ab8e8f3a6d6
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-12-13 20:01:59 +0100

    Created custom hook to fetch user data by id

---
+++ src/hooks/useGetUserById.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import axiosClient from "axiosClient";
+
+// define types for user document, loading, error
+
+interface User {
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  bio: string;
+  friends: string[];
+  ratings: string[];
+}
+
+// define types of returns
+interface UseGetUserByIdReturn {
+    fetchUser: (eventId :string ) => Promise<User | null>
+    user: User | null;
+    loading : boolean;
+    error: string | null;
+}
+
+const useGetUserById = (userId : string) : UseGetUserByIdReturn => {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchUser = async (userId : string) : Promise<User | null> => {
+        try {
+            setLoading(true)
+            const response = await axiosClient.get<User>(
+                `${import.meta.env.VITE_API_KEY}/user/${userId}`
+            )
+
+            // destructuring response
+            const userData = response.data?.data?.user
+
+            setUser(userData)
+            setError(null)
+            return userData
+        } catch (err) {
+            const axiosError = err as AxiosError<{ message: string }>;
+            setError(
+              axiosError.response?.data?.message ||
+                axiosError.message ||
+                "Unknown error"
+            );
+            setUser(null);
+            return null;
+        }finally{
+            setLoading(false)
+        }
+
+    }
+    useEffect(() =>{
+        fetchUser(userId)
+    }, [userId]) // Re-run if userId changes
+
+    return {fetchUser, user, loading, error}
+}
+
+export default useGetUserById
