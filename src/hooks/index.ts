315f41b33a45dd0e2dcc3683c2cac5d7aa766815
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-12-17 13:24:23 +0100

    Created custom hook for getting friend requests of logged user

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,6 @@ export * from "./useShowToast.tsx"
 export * from "./useAuthState.tsx"
 export * from "./useGetEventById.tsx"
 export * from "./useGetUserById.tsx"
+export * from "./useDeleteEventById.tsx"
+export * from "./useGetFriendRequests.tsx"
+
---
+++ src/hooks/useGetFriendRequests.tsx
@@ -0,0 +1,75 @@
+import { AxiosError } from "axios";
+import axiosClient from "axiosClient";
+import { useEffect, useState } from "react";
+
+export interface FriendRequests {
+  data: FriendRequest[];
+}
+
+export interface FriendRequest {
+  _id: string;
+  sender: User;
+  receiver: string;
+  createdAt: string;
+  state: string;
+  __v: number;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  bio: string | null;
+  profile_picture_path: string | null;
+  friends: string[];
+  ratings: string[];
+  __v: number;
+}
+
+export interface UseGetFriendRequestsReturn {
+  friendRequests: FriendRequest[] | null; // Friend requests can be null initially
+  loading: boolean; // Indicates if the data is being loaded
+  error: string | null; // Error message or null if no error
+}
+
+const useGetFriendRequests = (): UseGetFriendRequestsReturn => {
+  const [friendRequests, setFriendRequests] = useState<FriendRequest[] | null>(
+    null
+  );
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchRequests = async () => {
+    try {
+      setLoading(true);
+      const response = await axiosClient.get(
+        `${import.meta.env.VITE_API_KEY}/user/friend-request`
+      );
+
+      //destructuring data from response
+      const requestData = response.data?.data;
+      console.log(requestData)
+      setFriendRequests(requestData);
+    } catch (err) {
+      const axiosError = err as AxiosError<{ message: string }>;
+      setError(
+        axiosError.response?.data?.message ||
+          axiosError.message ||
+          "Unknown error"
+      );
+      setFriendRequests(null);
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  return { friendRequests, loading, error };
+};
+
+export default useGetFriendRequests;
