172ce4f0591cfa36e4479a0bbcafdd868081e6c2
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2024-12-18 10:10:08 +0100

    Adjusted for request document structure

--- src/components/ui/FriendRequestList.tsx
+++ src/components/ui/FriendRequestList.tsx
@@ -2,19 +2,38 @@ import { Stack } from "@Panda/jsx";
 import * as React from "react";
 import { FriendRequestCard } from "./FriendRequestCard";
 
-interface UserList {
-  userList: User[];
+interface RequestsData {
+  data: FriendRequest[];
+}
+
+export interface FriendRequest {
+  _id: string;
+  sender: User;
+  receiver: string;
+  createdAt: string;
+  state: string;
+  __v: number;
 }
 
 interface User {
-  user: { id: string; name: string; surname: string; imageUrl: string };
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  bio: string | null;
+  profile_picture_path: string | null;
+  friends: string[];
+  ratings: string[];
+  __v: number;
 }
 
-export const FriendRequestList: React.FC<UserList> = ({ userList }) => {
+export const FriendRequestList: React.FC<RequestsData> = ({ requestsData }) => {
+
   return (
     <Stack w="100%">
-      {userList.map(({ user }) => (
-        <FriendRequestCard key={user.id} user={user} />
+      {requestsData.map((request) => (
+        <FriendRequestCard key={request._id} request={request} />
       ))}
     </Stack>
   );
