04fb8b23919d3585a20181e7a16f327db6c2dcf9
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-01-06 12:21:41 +0100

    Implemented joining and leaving event

--- src/components/ui/EventToolbarEventDetail.tsx
+++ src/components/ui/EventToolbarEventDetail.tsx
@@ -2,28 +2,136 @@ import { HStack, Spacer } from "@Panda/jsx";
 import { Button } from "@ParkComponents/button";
 
 import { Text } from "@ParkComponents/text";
-import { CirclePlus } from "lucide-react";
+import axios from "axios";
+import axiosClient from "axiosClient";
+import { CirclePlus, DoorOpen } from "lucide-react";
 import * as React from "react";
+import { useShowToast } from "src/hooks";
+import useAuthStore from "src/store/authStore";
+
+interface EventCategory {
+  _id: string;
+  name: string;
+  description: string;
+  __v: number;
+}
+
+interface Attendee {
+  _id: string;
+  name: string;
+  surname: string;
+  username: string;
+  __v: number;
+}
+
+interface Author extends Attendee {
+  ratings: number[]; // Assuming ratings is an array of numbers
+  average_rating: number | null;
+}
+
+interface Event {
+  category: EventCategory;
+  _id: string;
+  name: string;
+  description: string;
+  date: string; // ISO 8601 string
+  private: boolean;
+  location: string;
+  attendees: Attendee[];
+  __v: number;
+  author: Author;
+}
 
 interface EventToolbarEventDetailProps {
-  eventName: string;
-  /*eventId: string;
-  userId: string;*/
+  event: Event;
 }
 
 export const EventToolbarEventDetail: React.FC<
   EventToolbarEventDetailProps
-> = ({ eventName /*eventId, userId*/ }) => {
+> = ({ event }) => {
+  const showToast = useShowToast();
+
+  const handleJoinEvent = async (eventId: string) => {
+    try {
+
+      const response = await axiosClient.post(
+        `${import.meta.env.VITE_API_KEY}/event/${eventId}/attendance`
+      );
+
+      if(response) return;
+      
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        showToast(
+          "Error",
+          `Creating event failed ${error.response?.data?.message || error.message}`,
+          "error"
+        );
+      } else {
+        showToast("Error", `Unexpected error occured : ${error}`, "error");
+      }
+    }
+  };
+
+  const handleLeaveEvent = async (eventId: string) => {
+    try {
+
+      const response = await axiosClient.delete(
+        `${import.meta.env.VITE_API_KEY}/event/${eventId}/attendance`
+      );
+
+      if(response){
+        showToast("Success", "You left an event", "success");
+        return;
+      }
+
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        showToast(
+          "Error",
+          `Creating event failed ${error.response?.data?.message || error.message}`,
+          "error"
+        );
+      } else {
+        showToast("Error", `Unexpected error occured : ${error}`, "error");
+      }
+    }
+  };
+
+  // calling authenticated user
+  const authUser = useAuthStore((state) => state.user);
+  console.log(event);
+
+  const [isAlreadyJoined, setIsAlreadyJoined] = React.useState(false);
+
+  // checking if authenticated user is in the attendees _ids
+  React.useEffect(() => {
+    event?.attendees?.forEach((attendee) => {
+      console.log(attendee);
+      if (attendee._id === authUser?._id) {
+        setIsAlreadyJoined(true);
+        return;
+      }
+    });
+  }, [event?.attendees]);
+
   return (
     <>
       <HStack mb="10px">
         <Text fontSize={{ base: "2xl", md: "3xl", xl: "4xl" }} fontWeight="500">
-          {eventName}
+          {event?.name}
         </Text>
         <Spacer />
-        <Button /*onClick={() => handleJoinEvent(eventId, userId)}*/>
-          <CirclePlus />
-          <Text>Join event</Text>
+        <Button
+          onClick={() =>
+            isAlreadyJoined
+              ? handleLeaveEvent(event?._id)
+              : handleJoinEvent(event?._id)
+          }
+        >
+          {isAlreadyJoined ? <DoorOpen /> : <CirclePlus />}
+
+          <Text>{isAlreadyJoined ? "Leave Event" : "Join event"}</Text>
         </Button>
       </HStack>
     </>
